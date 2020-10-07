e81fb6248a69166331fecbfbb52527393a2e5332
Mitrachet Frutiger <themic_za@gmail.com>
2020-10-07 16:09:21 +0700

    [ADD] order-info to dashboard.

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Grid } from '@material-ui/core';
 import AllProductWidget from './widgets/AllProductWidget';
+import OrderInfoWidget from './widgets/OrderInfoWidget'
 
 export default function DashboardPage() {
   return (
@@ -8,6 +9,9 @@ export default function DashboardPage() {
       <Grid container direction="column">
         <Grid item md={12}>
           <AllProductWidget title="อันดับรายการสินค้าที่มีการซื้อสูงสุด 5 อันดับ" />
+        </Grid>
+        <Grid item md={6}>
+          <OrderInfoWidget title="อันดับรายการสินค้าที่มีการซื้อสูงสุด 5 อันดับ" />
         </Grid>
       </Grid>
     </Container>
